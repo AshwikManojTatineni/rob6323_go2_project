8cdbfea81bca9abe202a2fad48ccdd5a99bcd93b
AshwikManojTatineni <ashwikmanojtatineni101@gmail.com>
2025-12-15 21:11:30 -0500

    Extra Credit: Actuator Friction

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
@@ -103,5 +103,5 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     # Additional reward scales
     orient_reward_scale = -5.0
     lin_vel_z_reward_scale = -0.1
-    dof_vel_reward_scale = -0.0001
+    dof_vel_reward_scale = -0.01
     ang_vel_xy_reward_scale = -0.001
